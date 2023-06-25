b737e628930d5023f355db3e25aa1b3cc3621b30
JpVargass <136398972+JpVargass@users.noreply.github.com>
2023-06-25 03:50:38 -0400

    Feliz trabajo terminado, con diseño y color fondo aplicación    <color name="paleta1">#025159</color> fondo imagenes      <color name="paleta4">#C4EEF2</color> color de botones     <color name="paleta5">#A67458</color>

--- app/src/main/java/cl/jpvs/modulo4_ejercicio9/MainActivity.java
+++ app/src/main/java/cl/jpvs/modulo4_ejercicio9/MainActivity.java
@@ -2,6 +2,7 @@ package cl.jpvs.modulo4_ejercicio9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import android.widget.Button;
@@ -11,9 +12,16 @@ import android.content.Intent;
 import android.view.View;
 
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    int imagen1 = R.drawable.baseline_yard_24;
+    int imagen2 = R.drawable.baseline_woman_24;
+    int imagen3 = R.drawable.baseline_theater_comedy_24;
+    int imagen4 = R.drawable.baseline_sick_24;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,41 +33,49 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initListeners() {
-        ImageButton boton1 = findViewById(R.id.button1);
-        ImageButton boton2 = findViewById(R.id.button2);
-        ImageButton boton3 = findViewById(R.id.button3);
-        ImageButton boton4 = findViewById(R.id.button4);
+        ImageView boton1 = findViewById(R.id.button1);
+        ImageView boton2 = findViewById(R.id.button2);
+        ImageView boton3 = findViewById(R.id.button3);
+        ImageView boton4 = findViewById(R.id.button4);
+        Button botonSalir = findViewById(R.id.buttonExit);
 
         boton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                moveToSecondActidadTwo();
+                moveToSecondActidadTwo(imagen1);
             }
         });
         boton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                moveToSecondActidadTwo();
+                moveToSecondActidadTwo(imagen2);
             }
         });
         boton3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                moveToSecondActidadTwo();
+                moveToSecondActidadTwo(imagen3);
             }
         });
         boton4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                moveToSecondActidadTwo();
+                moveToSecondActidadTwo(imagen4);
+            }
+        });
+
+        botonSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
 
-    public void moveToSecondActidadTwo(){
+    public void moveToSecondActidadTwo(int imagen){
         Intent intent = new Intent( MainActivity.this, SecondActivity.class);
+        intent.putExtra("Clave" , imagen);
         startActivity(intent);
             }
 
-
-}
+      }
--- app/src/main/java/cl/jpvs/modulo4_ejercicio9/SecondActivity.java
+++ app/src/main/java/cl/jpvs/modulo4_ejercicio9/SecondActivity.java
@@ -2,7 +2,13 @@ package cl.jpvs.modulo4_ejercicio9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +16,22 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        initListeners();
+        Intent intentGet = getIntent();
+        int idImage = intentGet.getIntExtra( "Clave",0);
+        Log.d("Second Activity", String.valueOf(idImage));
+
+        ImageView imageButton = findViewById(R.id.imageView);
+        imageButton.setImageResource(idImage);
+   }
+
+    private void initListeners() {
+        Button botonback = findViewById(R.id.buttonBack);
+        botonback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
